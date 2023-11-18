cd10cbd26eae144495fc9f2a5e009e4ed6f8c432
MarineSan <94964806+MarineSan@users.noreply.github.com>
2023-11-18 11:51:10 +0000

    Improvement of the visual : space under titles

--- routejet/core/models.py
+++ routejet/core/models.py
@@ -2,8 +2,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-
 class UserProfile(AbstractUser):
     additional_field = models.CharField(max_length=30, blank=True)
 
--- routejet/core/views.py
+++ routejet/core/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
 from core.forms import SignUpForm, LoginForm
 
-
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = 'core/register.html'  
