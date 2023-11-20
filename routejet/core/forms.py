58b4ff7020df8201b1867211144e93d378ef4922
MOHANAD JD ABULATIFA <82767666+mohanadabulatifa@users.noreply.github.com>
2023-11-20 18:42:54 +0000

    login and register styles fixed , and works correctly

--- routejet/core/forms.py
+++ routejet/core/forms.py
@@ -1,40 +1,42 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User 
-
+from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 class LoginForm(AuthenticationForm):
-  username = forms.CharField(widget=forms.TextInput(attrs={
-    'placeholder': 'Your username',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
-
-  password = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Your password',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Nombre de usuario',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
 
 class SignUpForm(UserCreationForm):
-  class Meta:
-    model = User
-    fields = ('username', 'email', 'password1', 'password2')
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
 
-  username = forms.CharField(widget=forms.TextInput(attrs={
-    'placeholder': 'Your username',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
-  email = forms.CharField(widget=forms.EmailInput(attrs={
-    'placeholder': 'Your email address',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Nombre de usuario',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
 
-  password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Your password',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'correo electrónico',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
 
-  password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-    'placeholder': 'Repeat your password',
-    'class': 'w-full py-4 px-6 rounded-xl'
-  }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repetir la contraseña',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
