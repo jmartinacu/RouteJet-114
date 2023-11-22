814f322784971909420dc6fbb0b0f1a2277a42c8
Joaquin Martin <jmartinacu2002@gmail.com>
2023-11-22 23:27:39 +0100

    fix: Cambiado formato codigo

--- routejet/core/urls.py
+++ routejet/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
-from core.views import signup, cerrarSesion
+from core.views import signup, logout_view
 from . import views
 from .forms import LoginForm
 
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
-    path('logout/',cerrarSesion, name='logout'),
+    path('logout/',logout_view, name='logout'),
 ]
--- routejet/core/views.py
+++ routejet/core/views.py
@@ -22,7 +22,7 @@ def signup(request):
   return render(request, 'core/signup.html', {
     'form': form
   })
-def cerrarSesion(request):
+def logout_view(request):
   logout(request)
   return redirect('/')
 
