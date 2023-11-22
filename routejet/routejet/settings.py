5d453c7cbe31e84b871ed6cf95488b2db2f7f556
Joaquin Martin <jmartinacu2002@gmail.com>
2023-11-22 11:38:29 +0100

    conf: Added LOGIN_REDIRECT_URL to settings.py

--- routejet/routejet/settings.py
+++ routejet/routejet/settings.py
@@ -124,15 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'core' / 'static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = "/"
 
 
 
