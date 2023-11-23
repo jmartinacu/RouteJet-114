f6cb10087f7d163e262cf974054e5cb6138f2def
MarineSan <94964806+MarineSan@users.noreply.github.com>
2023-11-23 16:18:15 +0000

    Displaying all products in the catalog, and a product details page upon clicking

--- routejet/store/views.py
+++ routejet/store/views.py
@@ -1,3 +1,10 @@
+from product.models import Product
 from django.shortcuts import render
 
-# Create your views here.
+def product_catalog(request):
+    products = Product.objects.all()
+    return render(request, 'templates/catalog.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, 'product/templates/product_detail.html', {'product': product})
