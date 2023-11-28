1cf1922242c18118297e37e4ed9c3a54c63571fc
Joaquin Martin <jmartinacu2002@gmail.com>
2023-11-28 19:33:32 +0100

    feat: Añadidos estilos catalogo ficha producto #17

--- routejet/product/views.py
+++ routejet/product/views.py
@@ -4,4 +4,4 @@ from .models import Product
 
 def product_detail(request, id, slug):
   product = get_object_or_404(Product, id=id, slug=slug, available=True)
-  return render('product/detail.html', {'product': product})
+  return render(request, 'product/detail.html', {'product': product})
