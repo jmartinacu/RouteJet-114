82166413d06a249a767fa708c665338c421a0917
jmartinacu <99323099+jmartinacu@users.noreply.github.com>
2023-11-28 20:10:11 +0100

    Update settings.py

--- routejet/routejet/settings.py
+++ routejet/routejet/settings.py
@@ -12,15 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-<<<<<<< HEAD
 import environ
 
 env = environ.Env(
   DEBUG=(bool, False)
 )
 environ.Env.read_env()
-=======
->>>>>>> develop
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
